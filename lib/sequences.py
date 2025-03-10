826c53a3ec6899c6180ecb54eee2fdb290d877a9
VictoriaCarvalho <vix219@icloud.com>
2025-03-10 12:08:52 -0500

    add lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence[:n])
+print_fibonacci(10)
